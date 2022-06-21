9533980957f864f83a090ac7307b0df7475109e8
Nando354 <fernandoaguirre@gmail.com>
2022-06-20 17:30:40 -0700

    Cleaning up older replaced code and adding new features. Organizing css to look cleaner

--- resources/js/script.js
+++ resources/js/script.js
@@ -16,43 +16,6 @@ let selectedThoughtKey = localStorage.getItem(LOCAL_STORAGE_SELECTED_THOUGHT_VAL
 
 let listToDelete = localStorage.getItem(LOCAL_STORAGE_THOUGHT_KEY);
 
-
-
-
-// console.log(thoughtLists);
-
-
-
-
-
-
-//TODO: Form entry, where value is entered and added as a list item when enter key is pressed
-
-// function scrollToThought() {
-//     var elmnt = document.getElementById("listAddButton");
-//     console.log('scroll initiated');
-//     elmnt.scrollIntoView({behavior: "smooth", block: "end"});
-// };
-
-//TODO: Eventlistener on first todo form where the value is entered into the form and added as a list item when the ENTER KEY is pressed. It calls the function formProcess which creates the thought list item.
-// newThoughtFormItem.addEventListener('keypress', e =>{
-//     if(e.key === "Enter"){
-//         e.preventDefault();
-//         formProcess();
-//         console.log('enter key on thought form was pressed');
-//         // scrollToThought();
-//     }
-// });
-//TODO: Form entry, where value is entered and added as a list item when button is clicked
-// newThoughtFormItem.addEventListener('submit', e =>{
-//         e.preventDefault();
-//         formProcess();
-//         console.log('submit button on thought form was clicked');
-//         // scrollToThought();
-// });
-
-
-
 //TODO: The form area where once a list item is typed and button is clicked or enter is pressed, the list value gets added to the createList function which holds the object and also pushed to Local Storage
 function formProcess(){
     console.log("formProcess function was set off")
@@ -68,30 +31,9 @@ function formProcess(){
     thoughtFormTxt.value = null;
   //pushes the list into the array lists or the object stored in local storage
     thoughtLists.push(thoughtList)
-  //call the renderlists function which will create the list items and save them in local storage
-    // console.log(listName);
-    // console.log(thoughtList);
-    // console.log(thoughtLists);
-    // console.log(thoughtLists.length)
-    // console.log(thoughtLists.slice(-1))
-    // saveAndRender();
-    // console.log(thoughtList.id)//pulls id of thought just processed
     var lastThoughtInForm = thoughtList.id;
-    // console.log(lastThoughtInForm);
-    // thoughtIdForm = thoughtList.id
-    // console.log(thoughtIdForm);
-    // console.log('formProcess Completed')
-    save();
-    // renderThoughts(lastThoughtInForm);
-    // transferThought(lastThoughtInForm);
-    
+    save();    
 }
-
-// let listName = thoughtFormTxt.value;
-// console.log(listName)
-
-// let responseName = responseFormTxt.value;
-
 
 //TODO: List text value is passed as a parameter and returns the object with the text value as a property of name
 function createList(name){
@@ -109,20 +51,24 @@ function saveAndRender(){
     renderThoughts();
 }
 
-  //TODO: Sets the key to the LOCAL STORAGE variable we named earlier and the object lists is stored to Local Storage as a string 
+//TODO: Sets the key to the LOCAL STORAGE variable we named earlier and the object lists is stored to Local Storage as a string 
 function save() {
     localStorage.setItem(LOCAL_STORAGE_THOUGHT_KEY, JSON.stringify(thoughtLists));
 }
-const arrayCount = thoughtLists[thoughtLists.length];
-// console.log(arrayCount)
-const lastArray = thoughtLists[thoughtLists.length - 1];
-// console.log(lastArray)
-// console.log(thoughtLists)
-// let lastOne = thoughtLists.slice(-1);
-// console.log(lastOne) 
-// console.log(lastOne[0].id)
-// console.log(thougthIdForm)
 
+function deletedLi(){
+  for(var i =0; i < thoughtLists.length; i++)
+    if(thoughtLists[i].id === deletedListId) {
+      //delete from the thoughts List
+      thoughtLists.splice(i,1);
+      //Save to Local Storage
+      save()
+      break;
+    }
+  saveAndRender();
+}
+const arrayCount = thoughtLists[thoughtLists.length];
+const lastArray = thoughtLists[thoughtLists.length - 1];
 
 //TODO: Will stop list from duplicating some list values entered on the <ul> unordered list with the ID name newlist, before we call a forEach function in renderLists()
 function clearElement(thoughtSections) {
@@ -130,11 +76,6 @@ function clearElement(thoughtSections) {
       thoughtSections.removeChild(thoughtSections.firstChild)
     }
 }
-
-// function transferThought(lastThoughtInForm){
-
-// }
-
 
 function renderThoughts(lastThoughtInForm) {
     console.log('renderThoughts function set off')
@@ -152,52 +93,41 @@ function renderThoughts(lastThoughtInForm) {
         //append span to li
         thoughtElement.appendChild(thoughtSpan);
         //remove button created
-        const responseBtn = document.createElement('button');
-        responseBtn.id = 'selectThought';
-        responseBtn.innerText = 'See Response';
+        const seeResponseBtn = document.createElement('button');
+        seeResponseBtn.id = 'selectThought';
+        seeResponseBtn.innerText = 'See Response';
         // removeBtn.classList.add('focus-visible-only')
-        thoughtElement.appendChild(responseBtn);
+        thoughtElement.appendChild(seeResponseBtn);
+        //delete thought/response
+        const deleteThoughtRespBtn = document.createElement('button');
+        deleteThoughtRespBtn.id = 'deleteBtn';
+        deleteThoughtRespBtn.innerText = 'X';
+        thoughtElement.appendChild(deleteThoughtRespBtn);
         // console.log(thoughtSections)
         thoughtSections.appendChild(thoughtElement)
         // console.log(thoughtList)
 
         //TODO: Event when the span text is clicked in the thoughts area, we want it to display the response to that particular thought.
-        responseBtn.addEventListener('click', e => {
+        seeResponseBtn.addEventListener('click', e => {
             console.log('response button has been clicked')
-            // if(e.target.tagName === "BUTTON"){
-            //   e.preventDefault();
-              // console.log("a button was clicked will not call toggler")
-            // } else if ((e.target.tagName === "SPAN")){
-            //   console.log(e.target.parentNode.id + " a span was clicked")
             //the button element with id selectThought from the thought list selected
               console.log(e.target)
               console.log(e.target.parentNode.firstChild.innerText)
-              // let thoughtSectionUl = document.getElementById('thoughtSection');
-              // console.log(thoughtSectionUl);
-              // var idInLi = document.getElementsByTagName('li')
-              // let idFromList = idInLi.getAttribute("id");
-              // console.log(idFromList);
-              // console.log(idInLi)
               //id of selected thought list item
               var selectedId = e.target.parentNode.id;
               //text of selected thought list tem
               var selectedText = e.target.parentNode.firstChild.innerText;
-              // var buttonText = e.target.span;
-              // var idLi = selectedId.getElementById('li').id;
-              // console.log(idLi)
-              // console.log(e.target.innerText)
-              // console.log(e.target.parentNode)
               console.log(selectedId)
-              // console.log(buttonText)
               console.log(selectedText)
               //id and text of selected thought list item is passed into the responseCard function
               responseCard(selectedId, selectedText);
-              // toggleOn();
-            //   toggler(e);
-              
-            // }
-          // }, false);
         }, false);
+
+        deleteThoughtRespBtn.addEventListener('click', e => {
+          deletedListId = e.target.parentNode.id;
+          console.log(thoughtList)
+          deletedLi(thoughtList);
+        })
     })
 }
 
@@ -212,43 +142,15 @@ function createResponse(name){
 //response area text area in form
 const newResponseFormItem = document.getElementById('responseForm');
 const pElemInModal = document.getElementById('modalTextSpan');
-const removeBtn = document.getElementById('removeResponse');
+const removeModalTxtBtn = document.getElementById('removeTxtInModal');
 let modal = document.getElementById('myModal');
 const thoughtTxtArea = document.getElementById('thoughtFormTxt');
 const responseTxtArea = document.getElementById('responseFormTxt');
-// console.log(thoughtTxtArea.value)
-//Enter to create the response
-// newResponseFormItem.addEventListener('keypress', e =>{
-//   if(e.key === "Enter"){
-//       e.preventDefault();
-//       responseFormProcess();
-//       console.log('enter key on response form');
-//       // scrollToThought();
-//   }
-// });
-//Click button to submit response
-// newResponseFormItem.addEventListener('submit', ev5 => {
-//     ev5.preventDefault();
-//     console.log(ev5.target)
-//     responseFormProcess();
-//     console.log('submit button on response form')
-//     // scrollToTask();
-//   });
-
-//Create one main button to enter both the thought and response in LS
-//Enter to create the response
-// newResponseFormItem.addEventListener('keypress', e =>{
-//   if(e.key === "Enter"){
-//     e.preventDefault();
-//     formProcess();
-//     responseFormProcess();
-//     console.log("text should pull")
-//     console.log(rspTxtArea.innerText);
-//   }
-// })
+const modalRspTxtArea = document.getElementById('modal-content');
+console.log(modalRspTxtArea)
 
 //FORM VALIDATION
-//Click button to submit both thought/response
+//Click button to submit both thought/response from form
 newResponseFormItem.addEventListener('submit', e =>{
   console.log('button to add thought/response set off')
   console.log(thoughtTxtArea.value)
@@ -266,6 +168,26 @@ newResponseFormItem.addEventListener('submit', e =>{
     responseFormProcess();
   }
 })
+//Press Enter key to submit both thought/response from form
+newResponseFormItem.addEventListener('keypress', ev5 =>{
+  if(ev5.key === "Enter"){
+  console.log('enter key clicked to add thought/response set off')
+  console.log(thoughtTxtArea.value)
+  console.log(responseTxtArea.value)
+  let thoughtTxt = thoughtTxtArea.value;
+  let respTxt = responseTxtArea.value;
+    if((ev5.key === "Enter") && (respTxt === "" || thoughtTxt === "")){
+      console.log('response is blank, fill in')
+      return false;
+    }
+    else {
+      console.log("no blanks")
+      ev5.preventDefault();
+      formProcess();
+      responseFormProcess();
+    }
+  }
+})
   
 //References the id responseFormTxt in index.html response form and pushes the typed text to createResponse() function
 function responseFormProcess(){
@@ -280,17 +202,11 @@ function responseFormProcess(){
     // console.log(thoughtLists);//the object thoughtLists is displayed
     
     let lastThoughtEntered = thoughtLists.slice(-1);
-    // console.log(lastThoughtEntered);
-    // console.log(lastThoughtEntered.id);
     for(var i = 0; i < thoughtLists.length; i++){
-        // console.log(thoughtLists[i]);
-        // console.log(response)
         itemLength = thoughtLists.length;
         // console.log(itemLength)
         let item = thoughtLists[i];
         if((i+1) == (itemLength)){
-          // console.log(item)
-          // console.log(item.id)
           let typedResponse = response;
           let selectedresponse = item.response.push(typedResponse);
         }
@@ -299,14 +215,18 @@ function responseFormProcess(){
     
 }
 
+let txtInModal01 = pElemInModal.innerText
+
 function toggleOn(){
-  // console.log(modal)
-  modal.style.display = 'block';
   console.log('ToggleOn was set off')
 }
 
 function toggleOff(){
-  modal.style.display = 'none';
+  // modal.style.display = 'none';
+  console.log(pElemInModal)
+  console.log(pElemInModal.innerText)
+  pElemInModal.innerText = "";
+  console.log(pElemInModal.innerText)
   console.log('ToggleOff was set off')
 }
 //Create the response pop up section here, pull the id's to match response and thought so they are relevent to one another.
@@ -335,123 +255,31 @@ function responseCard(selectedId, selectedText){
         console.log(responseInThoughts)
         console.log(responseInThoughts.name)
         console.log(thoughtId);
-
-       //*============
-        //Create the modal div
-        // let responseDiv = document.createElement('div');
-        // //give the div an id and class
-        // responseDiv.setAttribute('id', 'myModal');
-        // //create the modal content
-        // let responseContentDiv = document.createElement('div');
-        // responseContentDiv.classList.add('modal-content');
-        // //Create p element to place inside responseContentDiv
-        // let pElemInModal = document.createElement('p');
-        // //Set ID for P element
-        // pElemInModal.setAttribute('id', 'modalTextSpan');
-        // // let testText = document.createTextNode("for testing only");
-        // console.log(responseInThoughts.name)
-        // //Give the p element the response value as text
-        // pElemInModal.textContent = responseInThoughts.name;
-        // //Create the remove button
-        // const removeBtn = document.createElement('button');
-        // removeBtn.id = 'removeResponse';
-        // removeBtn.innerText = 'x';
-      //*=============
-
-        //Create var for p element
-        // const pElemInModal = document.getElementById('modalTextSpan')
         console.log(pElemInModal)
         //Give the p element the response value as text
         pElemInModal.textContent = responseInThoughts.name;
-        // //Create removeBtn event
-        removeBtn.addEventListener('click', e => {
+        console.log(responseInThoughts)
+
+        
+        console.log(modalRspTxtArea);
+        //Create removeBtn event
+        removeModalTxtBtn.addEventListener('click', e => {
           console.log("removeBtn was clicked")
           toggleOff();
         });
-        // //Append the p element to the responseContentDiv
-        // responseContentDiv.appendChild(pElemInModal);
-        // //Append removeBtn to responseContentDiv
-        // responseContentDiv.appendChild(removeBtn);
-        // //Append the responseContentDiv to the responseDiv
-        // responseDiv.appendChild(responseContentDiv);
-
-        //===========//
-        // //text from response is placed in a text Node
-        // let testText = document.createTextNode(responseToThought);
-        // //append response text as a child to the p element
-        // responseContentDiv.appendChild(testP);
-        // pElemInModal.appendChild(testText);
-        // responseDiv.appendChild(responseContentDiv);
-        // ===========//
-
-        // let responseArea = document.getElementById('thoughtSection');
-        // responseArea.appendChild(responseDiv);
-
-        // var modal = document.getElementById('myModal');
-        // modal.style.display = 'block';
-
-        var responseClass = document.getElementsByClassName('modal-content');
-        console.log(responseClass);
-
-        // var modalSpanClose = document.getElementsByClassName("close")[0];
-        // var modal = document.getElementById('myModal');
-
-        // console.log(modalSpanClose);
-        
-        // modalSpanClose = function() {
-        //   console.log('modal span was closed')
-        //   modal.style.display = "none";
-        // }
-        // modalClose = function() {
-        //   console.log('modalClose was set off')
-        //   modal.style.display = "none";
-        // }
-
 
         document.addEventListener('click', function(event){
           if(!event.target.closest("#myModal")
+          
           ){
+            console.log('clicked outside of modal')
           // closeModal()
           // toggleOff()
           }
            },
           false
            )
-
-      // function closeModal(){
-      //   modal.style.display = "none"
-      //   console.log('close modal function called')
-      // }
-        // window.onclick = function(event) {
-        //   console.log(event.target)
-        //   if (event.target !== (modal || responseClass)) {
-        //     console.log('modal style will be none')
-        //     // responseToThought = "";
-        //     // testText = "";
-        //     modal.style.display = "none";
-        //     // spanText = document.getElementById('modalTextSpan')
-        //     // console.log(spanText);
-        //     // spanText.textContent = "";
-        //   }
-        // }
       }
     }
-
   }
-  // let spans = thoughtSections.querySelectorAll("span");
-  // spans.forEach(function(span){
-  //   // if(span.className === "selected"){
-  //   if(span.innerText === selectedText){
-  //     idInNewThought = span.parentNode.id
-  //     console.log(idInNewThought); //id number as text, currently last list id in LS
-  //     console.log(selectedId)
-      // for(let list of lists) {
-      //   if(list.displayTasks === true && list.name === selectedText){
-      //     //selectedText was passed down as a parameter into the taskCard(selectedText) function
-      //     taskLiTitle.innerText = selectedText.toUpperCase();
-      //     taskCont.appendChild(taskLiTitle);
-      //   } 
-      // }
-  //   }
-  // });
 }
